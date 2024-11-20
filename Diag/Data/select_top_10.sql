a422c6dac44b42574a065036e83364e97c7af918
Leonid Dubersht <leonid.dubershtein@letme-agency.com>
2024-11-20 09:28:23 +0200

    Fix schema name

--- Diag/Data/select_top_10.sql
+++ Diag/Data/select_top_10.sql
@@ -1,36 +1,37 @@
 CREATE TABLE #Result (
+	SchemaName NVARCHAR(255),
     TableName NVARCHAR(255),
     ColumnName NVARCHAR(255),
     DataType NVARCHAR(50),
     Value NVARCHAR(MAX)
 );
 
+DECLARE @SchemaName NVARCHAR(255);
 DECLARE @TableName NVARCHAR(255);
 DECLARE @ColumnName NVARCHAR(255);
 DECLARE @DataType NVARCHAR(50);
 DECLARE @SQL NVARCHAR(MAX);
 
-DECLARE @Tables TABLE (TableName NVARCHAR(255));
-INSERT INTO @Tables (TableName)
+DECLARE @Tables TABLE (SchemaName NVARCHAR(255),TableName NVARCHAR(255));
+INSERT INTO @Tables (SchemaName,TableName)
 VALUES 
-    ('Table1'),
-    ('Table2'),
-    ('Table3'); 
+    ('dbo','T1'),
+    ('dbo','T2');
 
 
 DECLARE TableCursor CURSOR FOR 
-SELECT TableName 
+SELECT SchemaName,TableName 
 FROM @Tables;
 
 OPEN TableCursor;
-FETCH NEXT FROM TableCursor INTO @TableName;
+FETCH NEXT FROM TableCursor INTO @SchemaName,@TableName;
 
 WHILE @@FETCH_STATUS = 0
 BEGIN
     DECLARE ColumnCursor CURSOR FOR 
     SELECT COLUMN_NAME, DATA_TYPE
-    FROM INFORMATION_SCHEMA.COLUMNS
-    WHERE TABLE_NAME = @TableName
+    FROM INFORMATION_SCHEMA.COLUMNS 
+    WHERE TABLE_NAME = @TableName AND TABLE_SCHEMA = @SchemaName
           AND DATA_TYPE IN ('varchar', 'nvarchar', 'char', 'nchar', 'text', 'int', 'bigint', 'smallint', 'tinyint');
 
     OPEN ColumnCursor;
@@ -41,7 +42,7 @@ BEGIN
         -- create query
         SET @SQL = N'
             INSERT INTO #Result (TableName, ColumnName, DataType, Value)
-            SELECT TOP 10 
+            SELECT TOP 2 
                 ''' + @TableName + ''',
                 ''' + @ColumnName + ''',
                 ''' + @DataType + ''',
@@ -49,6 +50,8 @@ BEGIN
             FROM ' + QUOTENAME(@TableName) + '
             WHERE ' + QUOTENAME(@ColumnName) + ' IS NOT NULL;
         ';
+
+		-- print
 
         -- exec
         EXEC sp_executesql @SQL;
@@ -59,7 +62,7 @@ BEGIN
     CLOSE ColumnCursor;
     DEALLOCATE ColumnCursor;
 
-    FETCH NEXT FROM TableCursor INTO @TableName;
+    FETCH NEXT FROM TableCursor INTO @SchemaName, @TableName;
 END;
 
 CLOSE TableCursor;
