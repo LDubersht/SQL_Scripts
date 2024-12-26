9759c10b4d96b6d6a64dfd1e9ef8c2cf1c5c771a
Leonid Dubersht <leonid.dubershtein@letme-agency.com>
2024-12-26 13:11:46 +0200

    change

--- Maintain/Compactizator.sql
+++ Maintain/Compactizator.sql
@@ -4,11 +4,12 @@ DECLARE @FileName NVARCHAR(128),
 		@DestFileSizeMB int,
         @AvailableSpaceMB FLOAT,
         @ShrinkDataStepSizeMB INT = 500,
-        @ShrinkLogStepSizeMB INT = 1500,
+        @ShrinkLogStepSizePercent INT = 10,
         @WaitTimeAfterStep INT = 10, -- in seconds
         @WaitTimeAfterCycle INT = 60, -- in seconds
 		@DataFileThreshold INT = 5, --for start shrink in persent
 		@LogFileThreshold INT = 15, --for start shrink in persent
+		@FileSizeLimitMB INT = 10000, -- min file size 
 		@StartTime DATETIME,
 		@ElapsedTime INT;
 
@@ -27,37 +28,51 @@ BEGIN
 
     WHILE @@FETCH_STATUS = 0
     BEGIN
-        IF (@AvailableSpaceMB / @FileSizeMB) * 100 > @DataFileThreshold and @FileType = 0
-        BEGIN
-            SET @StartTime = GETDATE();
-			SET @DestFileSizeMB = CAST(@FileSizeMB - @ShrinkDataStepSizeMB AS INT)
-			IF @FileSizeMB>@DestFileSizeMB
-				DBCC SHRINKFILE (@FileName, @DestFileSizeMB);
-
-            SET @ElapsedTime = DATEDIFF(SECOND, @StartTime, GETDATE());
-            RAISERROR ('Shrink step completed for file %s. Time taken: %d seconds.', 10, 1, @FileName, @ElapsedTime) WITH NOWAIT;
-
-            WAITFOR DELAY '00:00:10';
-        END
-		ELSE IF (@AvailableSpaceMB / @FileSizeMB) * 100 > @LogFileThreshold and @FileType = 1
+        IF @FileType = 0 
 		BEGIN
-            SET @StartTime = GETDATE();
-			SET @DestFileSizeMB = CAST(@FileSizeMB-@ShrinkLogStepSizeMB AS INT)
-			IF @FileSizeMB>@DestFileSizeMB
+			SET @DestFileSizeMB = CAST(@FileSizeMB - @ShrinkDataStepSizeMB AS INT)
+			IF (@AvailableSpaceMB / @FileSizeMB) * 100 > @DataFileThreshold 
+				AND  @DestFileSizeMB > @FileSizeLimitMB
+				AND  @FileSizeMB > @DestFileSizeMB
+			BEGIN
+				SET @StartTime = GETDATE();
+				
 				DBCC SHRINKFILE (@FileName, @DestFileSizeMB);
 
-            SET @ElapsedTime = DATEDIFF(SECOND, @StartTime, GETDATE());
-            RAISERROR ('Shrink step completed for file %s. Time taken: %d seconds.', 10, 1, @FileName, @ElapsedTime) WITH NOWAIT;
+				SET @ElapsedTime = DATEDIFF(SECOND, @StartTime, GETDATE());
+				RAISERROR ('Shrink step completed for file %s. Time taken: %d seconds.', 10, 1, @FileName, @ElapsedTime) WITH NOWAIT;
 
-            WAITFOR DELAY '00:00:10';
-        END
-        ELSE
-        BEGIN
-            RAISERROR ('File %s has less than 5%% free space. Waiting for the next cycle.', 10, 1, @FileName) WITH NOWAIT;
-            WAITFOR DELAY '00:01:00';
-        END
+				WAITFOR DELAY '00:00:01';
+			END
+			ELSE 
+		    BEGIN
+				RAISERROR ('DataFile %s has less than free space. Waiting for the next cycle.', 10, 1, @FileName) WITH NOWAIT;
+				WAITFOR DELAY '00:01:01';
+			END
+		END
+		IF  @FileType = 1
+		BEGIN
+			SET @DestFileSizeMB = CAST(@FileSizeMB*(100-@ShrinkLogStepSizePercent)/100 AS INT)
+			IF (@AvailableSpaceMB / @FileSizeMB) * 100 > @LogFileThreshold 
+				AND @FileSizeMB>@DestFileSizeMB AND @DestFileSizeMB > @FileSizeLimitMB
+			BEGIN
+				SET @StartTime = GETDATE();
+				
+				DBCC SHRINKFILE (@FileName, @DestFileSizeMB);
 
-        FETCH NEXT FROM FileCursor INTO @FileName, @FileType, @FileSizeMB, @AvailableSpaceMB;
+				SET @ElapsedTime = DATEDIFF(SECOND, @StartTime, GETDATE());
+				RAISERROR ('Shrink step completed for file %s. Time taken: %d seconds.', 10, 1, @FileName, @ElapsedTime) WITH NOWAIT;
+
+				WAITFOR DELAY '00:00:01';
+			END
+			ELSE
+			BEGIN
+				RAISERROR ('File %s has less than free space. Waiting for the next cycle.', 10, 1, @FileName) WITH NOWAIT;
+				WAITFOR DELAY '00:01:01';
+			END
+		END
+        
+		FETCH NEXT FROM FileCursor INTO @FileName, @FileType, @FileSizeMB, @AvailableSpaceMB;
     END
 
     CLOSE FileCursor;
